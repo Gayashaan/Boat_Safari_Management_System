1f8b59dd4826fd8e110166ea6f42b7c7d71b69ce
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2023-05-23 20:17:31 +0530

    Update the data base creation in the databse.php

--- admin/database.php
+++ admin/database.php
@@ -20,11 +20,11 @@
     //query for insert data if table is empty
     $insertData = "
     INSERT INTO admin(adminID, fname, lname, email, pwd, cnumber) VALUES
-    (1, 'Dinuvi', 'Asithma', 'dinuviasithma@gmail.com', '123', 718823436),
-    (2, 'Gayashaan', 'Krishnamoorthy', 'gayashaan49@gmail.com', '123', 718823436),
-    (3, 'shenal', 'somaweera', 'shenalsomaweera@gmail.com', '123', 1324654),
-    (4, 'Sasiru', 'Gunathilaka', 'gunathilakasasiya@gmail.com', '123', 465465),
-    (5, 'Oshada', 'Dhahanayaka', 'oshadadhahanayaka2002@gmail.com', '123', 465465)";
+    (1, 'Dinuvi', 'Asithma', 'dinuviasithma@gmail.com', 'admin', 0771234569),
+    (2, 'Gayashaan', 'Krishnamoorthy', 'gayashaan49@gmail.com', 'admin', 0771234569),
+    (3, 'shenal', 'somaweera', 'shenalsomaweera@gmail.com', 'admin', 0774587963),
+    (4, 'Sasiru', 'Gunathilaka', 'gunathilakasasiya@gmail.com', 'admin', 0771458963),
+    (5, 'Oshada', 'Dhahanayaka', 'oshadadhahanayaka2002@gmail.com', 'admin', 0774589632)";
 
     
 
