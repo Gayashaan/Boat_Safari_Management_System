971beaed09e5ff6e901de4e7cf47f44dea6f7ca5
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2023-05-23 12:23:41 +0530

    updfate in relord time

--- main/adminloging.php
+++ main/adminloging.php
@@ -21,7 +21,7 @@
             }else{
                 
                 echo "<script> alert('No such records');</script>";
-                header("Refresh: 1; URL = adminloging.php");
+                header("Refresh: 0; URL = adminloging.php");
             }
                     
         }
