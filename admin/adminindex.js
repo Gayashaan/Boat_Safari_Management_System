beeaf92b39d4409809cc2124b220b46251c5926a
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2023-06-01 22:00:06 +0530

    Login page added and reservation page added

--- admin/adminindex.js
+++ admin/adminindex.js
@@ -1,14 +1,14 @@
 function confirmUpdate(name) {
-    var confirmed = confirm('Are you sure you want to update ' + name +  '\'s profile?');
-    if (!confirmed) {
-      return false;
-    }
+  var confirmed = confirm('Are you sure you want to update ' + name +  '\'s profile?');
+  if (!confirmed) {
+    return false;
   }
+}
 
 
 function confirmDelete() {
-    var confirmed = confirm('Are you sure you want to delete this profile?');
-    if (!confirmed) {
-      return false;
-    }
+  var confirmed = confirm('Are you sure you want to delete this profile?');
+  if (!confirmed) {
+    return false;
   }
+}
