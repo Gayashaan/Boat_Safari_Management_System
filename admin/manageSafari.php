cbcdf1808bd1932f737af59382feb8a20760d272
Shenal Somaweera <it22337276@my.sliit.lk>
2023-06-10 21:46:13 +0530

    Crud done

--- admin/manageSafari.php
+++ admin/manageSafari.php
@@ -23,7 +23,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/common.css">
-    <link rel="stylesheet" href="css/manageSafari.css">
+    <link rel="stylesheet" href="css/managesafari.css">
+    
      <!-- This are icon for dash board -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -94,7 +95,7 @@
                                         <td> 
                                             <div class="opBtns">
                                                 <button id="vwBtn"><a href="updateSafari.php?updateid='.$ID.'">View</a></button>
-                                                <button id="dlBtn"><a href="deleteSafari.php?deleteid='.$ID.'">Delete</a></button>
+                                                <button id="dlBtn"><a href="deletesafari.php?deleteid='.$ID.'">Delete</a></button>
                                             </div>
                                         </td>
                                     </tr>';
