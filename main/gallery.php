a52851bb499bbcfd70eae47bd002d46be4213225
Oshadha Dahanayaka <it22915672@my.sliit.lk>
2023-06-10 22:06:56 +0530

    oo

--- main/gallery.php
+++ main/gallery.php
@@ -23,12 +23,12 @@
         <div class="container">
         <h1>Image Gallery</h1>
         <div class="gallery">
-            <img src="Images/userview/3d_600x400.jpg">
-            <img src="Images/userview/4d_600x400.jpg">
-            <img src="Images/userview/boat-g50ee0137d_1280_600x400.jpg">
-            <img src="Images/userview/6d 600.jpg">
-            <img src="Images/userview/yacht-rental-singapore-seek-sophie-yacht-water-play.jpg" width="600px" height="400px">
-            <img src="Images/userview/Experience-Boat-Safari-Kaziranga-Range-Assam-7-1160x650.jpg" width="600px" height="400px">
+            <img src="images/userview/3d_600x400.jpg">
+            <img src="images/userview/4d_600x400.jpg">
+            <img src="images/userview/boat-g50ee0137d_1280_600x400.jpg">
+            <img src="images/userview/6d 600.jpg">
+            <img src="images/userview/yacht-rental-singapore-seek-sophie-yacht-water-play.jpg" width="600px" height="400px">
+            <img src="images/userview/Experience-Boat-Safari-Kaziranga-Range-Assam-7-1160x650.jpg" width="600px" height="400px">
         </div>
         </div>
                 
