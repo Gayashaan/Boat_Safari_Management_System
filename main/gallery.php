5ad0722589be5a513168d2f214e50a45729f8f27
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2023-06-10 14:04:24 +0530

    oshada gallery page

---
+++ main/gallery.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/gallery.css">
+    
+    <title>User Profile</title>
+</head>
+
+<body>
+
+    <?php include("header.php"); ?>
+    
+    <div class="container">
+
+        <div class="wrap">
+            
+            <h1>This is the galler page</h1>
+            <br>
+            <h1>paset your code to this section</h1>
+                
+            
+        </div>
+    </div>
+    
+    
+    
+</body>
+</html>
