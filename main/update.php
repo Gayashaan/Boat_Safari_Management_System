8479da39ebdc4bb701dddbf8ce190b98d2bb8e73
it22136824Sasiru <it22136824@my.sliit.lk>
2023-06-11 19:28:34 +0530

    update wada na bn

--- main/update.php
+++ main/update.php
@@ -2,29 +2,29 @@
 
       include_once("../admin/config.php");
       
-      
+     if(isset($_GET['updatebtn'])) {
 
-     $userID = $_GET['updateid'];
-    
-     $fname=$_POST['fname']; 
-      $lname=$_POST['lname']; 
-      $email=$_POST['email'];
-      $cnumber=$_POST['cnumber'];
-      $Address=$_POST['Address'];
-      $pwd=$_POST['pwd'];
+        $userID = $_GET['updateid'];
+       $fname=$_POST['fname']; 
+       $lname=$_POST['lname']; 
+        $email=$_POST['email'];
+       $cnumber=$_POST['cnumber'];
+       $Address=$_POST['Address'];
+       $pwd=$_POST['pwd'];
 
+       $sql="UPDATE user set userID='$userID',fname=' $fname',lname=' $lname',email='$email',pwd='$pwd',cnumber=' $cnumber',Address='$Address'
+       WHERE userID = '$userID'";
 
-     $sql="UPDATE 'user' set userID=$userID,fname='$fname',lname='$lname',email='$email',pwd='$pwd',cnumber='$cnumber',Address=' $Address'
-     where userID='$userID'  ";
-     $result=mysqli_query($conn,$sql);
-     if($result)
-     {
-        echo"Updated successfully";
+       if($conn->query($sql)===TRUE){
+        echo" Updated successfully";
+       }
+       else{
+        echo"Error:".$conn->error;
+       }
 
      }
-     else{
-        die(mysqli_error($conn));
-     }
+
+     
     
     
 ?>
