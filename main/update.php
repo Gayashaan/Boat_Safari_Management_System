008fbc3705ff312516d0fa5dcb42b602e30bab73
it22136824Sasiru <it22136824@my.sliit.lk>
2023-06-11 19:48:52 +0530

    Update update.php

--- main/update.php
+++ main/update.php
@@ -2,16 +2,18 @@
 
       include_once("../admin/config.php");
       
-     if(isset($_GET['updatebtn'])) {
+     
 
         $userID = $_GET['updateid'];
-       $fname=$_POST['fname']; 
-       $lname=$_POST['lname']; 
-        $email=$_POST['email'];
+        $fname=$_POST['fname']; 
+        $lname=$_POST['lname']; 
+       $email=$_POST['email'];
        $cnumber=$_POST['cnumber'];
        $Address=$_POST['Address'];
        $pwd=$_POST['pwd'];
 
+
+       
        $sql="UPDATE user set userID='$userID',fname=' $fname',lname=' $lname',email='$email',pwd='$pwd',cnumber=' $cnumber',Address='$Address'
        WHERE userID = '$userID'";
 
@@ -21,8 +23,10 @@
        else{
         echo"Error:".$conn->error;
        }
+       mysqli_close($conn);
 
-     }
+     
+
 
      
     
